f133c2a9ba53ca62bb425f750943a7ff299ea1b9
Aleksandr <tkdespadon@mail.ru>
2022-08-09 11:40:37 +0300

    init commit

---
+++ zd3/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int a = (n / 2 * 2);
+if (a == n) {
+Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
---
+++ zd4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <=n; i+=2) {
+        Console.WriteLine(i);
+}
