2fc3ebf39ce5601321df435a78d5feac04800f37
Aleksandr <tkdespadon@mail.ru>
2022-08-11 09:17:32 +0300

    2семенар дз1i2

---
+++ 2zd1/Program.cs
@@ -0,0 +1,8 @@
+﻿void learnSecond(){
+Random random = new Random();
+int valeue = random.Next(100, 1000);
+Console.WriteLine(valeue);
+int number2 = ((valeue / 10) % 10);
+Console.WriteLine(number2);
+}
+learnSecond();
