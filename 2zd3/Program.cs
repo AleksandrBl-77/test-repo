313099b3d6572571c5078d06e07d35452336ab70
Aleksandr <tkdespadon@mail.ru>
2022-08-12 11:00:05 +0300

    2семенар дз1i2i3

---
+++ 2zd3/Program.cs
@@ -0,0 +1,11 @@
+﻿void knowdayOff(){
+Console.Write("Введите номер дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number <= 5){
+    Console.WriteLine("нет");
+}
+else{
+    Console.WriteLine("да");
+}
+}
+knowdayOff();
